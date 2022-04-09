94c1ab17e9c1bc56832f07fdc5b4c8f4eafc0aa5
thijsBoet <m.boet@chello.nl>
2022-04-09 17:23:58 +0200

    building nav component

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,15 @@
+import requests from '../utils/requests';
+
 const Nav = () => {
 	return (
 		<nav>
-			<div>
-				
+			<div className=''>
+				{Object.entries(requests).map(([key, { title, url }]) => (
+					<h2>{title}</h2>
+				))}
 			</div>
 		</nav>
 	);
-}
+};
 
 export default Nav;
